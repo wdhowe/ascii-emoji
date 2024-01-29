60580c33180705caa20c50f03db9ab7ec622d968
Bill Howe <wdhowe@protonmail.com>
2024-01-29 17:42:09 -0600

    project cleanup.

--- doc/intro.md
+++
@@ -1,5 +0,0 @@
-# Introduction to ascii-emoji
-
-Use ASCII style emojis in Clojure! For when you just need to flip a table or shrug.
-
-The emojis can be accessed directly via a map or via helper functions to list names, describe, show, or search the map.
--- project.clj
+++ project.clj
@@ -1,9 +1,15 @@
 (defproject ascii-emoji "0.1.0"
+  
+  ;;; Project Metadata
   :description "Use ASCII Emoji, in Clojure."
   :url "https://github.com/wdhowe/ascii-emoji"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
+  
+  ;;; Dependencies, Plugins
   :dependencies [[org.clojure/clojure "1.10.0"]]
   :plugins [[lein-codox "0.10.7"]]
+  
+  ;;; Running Project Code
   :repl-options {:init-ns ascii-emoji.core}
   :resource-paths ["resources"])
